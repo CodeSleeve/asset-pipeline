e3977f90c5d51042d8bec65bd320297396bd0d41
Eric Villard <dev@eviweb.fr>
2013-10-31 00:06:45 +0100

    fix: non existing config field in CssUrlFilter
    Signed-off-by:Eric Villard <dev@eviweb.fr>

--- src/Codesleeve/AssetPipeline/Filters/CssUrlFilter.php
+++ src/Codesleeve/AssetPipeline/Filters/CssUrlFilter.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 namespace Codesleeve\AssetPipeline\Filters;
 
 use Assetic\Asset\AssetInterface;
@@ -14,79 +14,79 @@ class CssUrlFilter implements FilterInterface
      * @var object
      */
     private $app;
-    
+
     /**
      * current path handler
      * 
      * @var string
      */
     private $path;
-    
+
     /**
      * environments
      * 
      * @var array
      */
     private $environments;
-    
+
     /**
      * asset paths
      * 
      * @var array
      */
     private $paths;
-    
+
     /**
      * routing prefix
      * 
      * @var array
      */
     private $prefix;
-    
+
     /**
-	 * [initialize description]
-	 * @param  {[type]} $app [description]
-	 * @return {[type]}      [description]
-	 */
-	public function initialize($app)
-	{
-		$this->app = $app;
+     * [initialize description]
+     * @param  {[type]} $app [description]
+     * @return {[type]}      [description]
+     */
+    public function initialize($app)
+    {
+        $this->app = $app;
         $this->paths = $app['config']->get('asset-pipeline::paths');
         $this->prefix = $app['config']->get('asset-pipeline::routing.prefix', '/assets') . '/';
-	}
-    
+    }
+
     /**
-	 * [__construct description]
-	 * @param  string|array $environments list of environments we should minify on
-	 * @return null
-	 */
-	public function __construct($environments = 'production')
-	{
-		$this->environments = !is_array($environments) ? array($environments) : $environments;
-	}
-    
-	/**
-	 * [filterLoad description]
-	 * @param  AssetInterface $asset [description]
-	 * @return [type]                [description]
-	 */
+     * [__construct description]
+     * @param  string|array $environments list of environments we should minify on
+     * @return null
+     */
+    public function __construct($environments = 'production')
+    {
+        $this->environments = !is_array($environments) ? array($environments) : $environments;
+    }
+
+    /**
+     * [filterLoad description]
+     * @param  AssetInterface $asset [description]
+     * @return [type]                [description]
+     */
     public function filterLoad(AssetInterface $asset)
     {
 
     }
- 
- 	/**
- 	 * [filterDump description]
- 	 * @param  AssetInterface $asset [description]
- 	 * @return [type]                [description]
- 	 */
+
+    /**
+     * [filterDump description]
+     * @param  AssetInterface $asset [description]
+     * @return [type]                [description]
+     */
     public function filterDump(AssetInterface $asset)
     {
         if ($this->shouldProcess()) {
             $content = $asset->getContent();
             $this->path = $this->calculatePath($asset);
             $asset->setContent(
-                CssUtils::filterUrls($content, function($matches){
+                CssUtils::filterUrls($content, function($matches) {
                     $urlparts = explode('/', $matches['url']);
                     $pathparts = explode('/', $this->path);
                     $filepath = array();
@@ -99,7 +99,7 @@ class CssUrlFilter implements FilterInterface
                     }
                     return str_replace(
                         $matches['url'],
-                        implode('/', $pathparts).'/'.implode('/', $filepath),
+                        implode('/', $pathparts) . '/' . implode('/', $filepath),
                         $matches[0]
                     );
                 })
@@ -119,39 +119,37 @@ class CssUrlFilter implements FilterInterface
         $filepath = $this->normalizePath(
             str_replace($this->app['path.base'] . '/', '', $asset->getSourceRoot())
         );
-        foreach($this->paths as $key => $path)
-		{
+        foreach ($this->paths as $key => $path) {
             $compare = is_integer($key) ? $path : $key;
-			if (substr($filepath, 0, strlen($compare)) == $compare) {
-				$filepath = substr($filepath, strlen($compare));
-				continue;
-			}
-		}
+            if (substr($filepath, 0, strlen($compare)) == $compare) {
+                $filepath = substr($filepath, strlen($compare));
+                continue;
+            }
+        }
         $filepath = $this->prefix . ltrim($filepath, '/');
-        if (isset($this->app['url']))
-		{
-			return app('url')->asset($filepath, $this->config->get('secure'));
-		}
+        if (isset($this->app['url'])) {
+            return app('url')->asset($filepath, $this->app['config']->get('secure'));
+        }
         return $filepath;
     }
-    
+
     /**
-	 * This is used to convert any Windows slashes into unix style slashes
-	 *
-	 * @param [type] $path [description]
-	 * @return [type] 	   [description]
-	 */
-	protected function normalizePath($path)
-	{
-		return str_replace('\\', '/', $path);
-	}
-    
+     * This is used to convert any Windows slashes into unix style slashes
+     *
+     * @param [type] $path [description]
+     * @return [type] 	   [description]
+     */
+    protected function normalizePath($path)
+    {
+        return str_replace('\\', '/', $path);
+    }
+
     /**
      * [shouldMinify description]
      * @return {[type]} [description]
      */
-	protected function shouldProcess()
-	{
-		return in_array($this->app['env'], $this->environments);
-	}
+    protected function shouldProcess()
+    {
+        return in_array($this->app['env'], $this->environments);
+    }
 }
