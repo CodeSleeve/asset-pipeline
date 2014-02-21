301019b3c8cb4dc2cfa4060dd2ec0d69f439a995
Kelt Dockins <kelt@dockins.org>
2014-02-20 22:53:42 -0600

    https://github.com/CodeSleeve/asset-pipeline/issues/120

--- src/Codesleeve/AssetPipeline/Commands/AssetsCleanCommand.php
+++ src/Codesleeve/AssetPipeline/Commands/AssetsCleanCommand.php
@@ -21,23 +21,37 @@ class AssetsCleanCommand extends Command
     protected $description = "Cleans out all your cached assets";
 
     /**
+     * Construct a new AssetsCleanCommand
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->asset = \App::make('asset');
+
+        // force concatenation off, so we can get
+        // a big list of all the files required
+        // in this manifest file
+        $config = $this->asset->getConfig();
+        $config['concat'] = array();
+        $this->asset->setConfig($config);        
+    }
+
+    /**
      * Execute the console command.
      *
      * @return void
      */
     public function fire()
     {
+        $recursive = $this->option('recursive') === true;
+        $this->verbose = $this->option('verbose') === true;
+
         $files = $this->option('file');
-        $asset = \App::make('asset');
 
         foreach ($files as $file)
         {
-            $absolutePath = $asset->parser->absoluteFilePath($file);
-            $removed = $asset->generator->cached($absolutePath)->remove();
-
-            if ($removed === false) {
-                echo PHP_EOL . "<warning> failed to find/remove cache for {$absolutePath}" . PHP_EOL;
-            }
+            $this->removeAssetCache($file, $recursive);
         }
 
         $this->line('');
@@ -49,6 +63,43 @@ class AssetsCleanCommand extends Command
     }
 
     /**
+     * Removes the AssetCache for this file
+     * 
+     * @param  [type] $file      [description]
+     * @param  [type] $recursive [description]
+     * @return [type]            [description]
+     */
+    protected function removeAssetCache($file, $recursive)
+    {
+        $files = $this->asset->isJavascript($file) ? $this->asset->parser->javascriptFiles($file) : $this->asset->parser->stylesheetFiles($file);
+
+        array_push($files, $this->asset->parser->absoluteFilePath($file));
+
+        foreach ($files as $file)
+        {
+            $removed = $this->asset->generator->cachedFile($file)->remove();
+
+            if ($removed === false) {
+                $this->writeln(PHP_EOL . "<warning> failed to find/remove cache for {$file}");
+            }
+        }
+    }
+
+    /**
+     * Output messages to the user if verbose is on
+     * 
+     * @param  `[type] $message [description]
+     * @return [type]          [description]
+     */
+    protected function writeln($message)
+    {
+        if ($this->verbose)
+        {
+            echo $message . PHP_EOL;
+        }
+    }
+
+    /**
      * Get the console command options.
      *
      * @return array
@@ -57,6 +108,8 @@ class AssetsCleanCommand extends Command
     {
         return array(
             array('file', 'f', InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'This is files that will have the assets cleaned.', array('application.js', 'application.css')),
+            array('recursive', 'r', InputOption::VALUE_OPTIONAL, 'Should we recursively remove all cached files the manifest requires? Default is true', true),
+          //  array('verbose', 'v', InputOption::VALUE_OPTIONAL, 'Should we be extra noisy? Default is false', false),
         );
     }
 }
